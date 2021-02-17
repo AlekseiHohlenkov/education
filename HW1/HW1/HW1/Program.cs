56ad3d0af0f4cc7f29dc900ddbcc1823543fd2cc
Aleksei Hohlenkov <aswyga@outlook.com>
2021-02-17 19:45:51 +0200

    HW1

---
+++ HW1/HW1/HW1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Я хоче стать програмистом потому, что у меня не было професии к которой я мог относиться как кремеслу");
+        }
+    }
+}
